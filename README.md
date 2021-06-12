fa32334798b2337649a34e1b6741ec7d0f3b5793
Dominik Biedebach <dominik@bdbch.com>
2021-06-12 04:53:59 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# FlyntWP Docker Starter
+
+> A starter kit to quickly setup a new Wordpress + FlyntWP project
+
+## Setup
+
+This repository includes multiple docker containers for working locally. **Deployment via Docker is not planned yet but may be considered later**
+
+You can set up this project quickly via `docker-compose` which will spin up the following containers:
+
+- `builder` - includes node.js and is used to build assets locally while working on the files via a watcher
+- `wordpress` - this includes the webserver and wordpress plus handles the initial building of all assets via node 14
+- `db` - this includes the development database
+
+To make sure node_module are installed, run `docker-compose exec builder yarn install`.
+
+## After the docker setup
+
+After setting up the project, the website should be available at `http://localhost`.
+
+Make sure to the start the bundler via `docker-compose exec builder yarn start`.
---
+++ wp-content/themes/flynt/Components/SliderImages/script.js
@@ -0,0 +1,60 @@
+import "core-js/es/number";
+import "swiper/swiper-bundle.css";
+
+import $ from "jquery";
+import Swiper, { A11y, Autoplay, Navigation } from "swiper/swiper.esm";
+
+Swiper.use([Navigation, A11y, Autoplay]);
+
+class SliderImages extends window.HTMLDivElement {
+  constructor(...args) {
+    const self = super(...args);
+    self.init();
+    return self;
+  }
+
+  init() {
+    this.$ = $(this);
+    this.props = this.getInitialProps();
+    this.resolveElements();
+  }
+
+  getInitialProps() {
+    let data = {};
+    try {
+      data = JSON.parse($('script[type="application/json"]', this).text());
+    } catch (e) {}
+    return data;
+  }
+
+  resolveElements() {
+    this.$slider = $("[data-slider]", this);
+    this.$buttonNext = $('[data-slider-button="next"]', this);
+    this.$buttonPrev = $('[data-slider-button="prev"]', this);
+  }
+
+  connectedCallback() {
+    this.initSlider();
+  }
+
+  initSlider() {
+    const { options } = this.props;
+    const config = {
+      navigation: {
+        nextEl: this.$buttonNext.get(0),
+        prevEl: this.$buttonPrev.get(0),
+      },
+      a11y: options.a11y,
+    };
+    if (options.autoplay && options.autoplaySpeed) {
+      config.autoplay = {
+        delay: options.autoplaySpeed,
+      };
+    }
+    this.slider = new Swiper(this.$slider.get(0), config);
+  }
+}
+
+window.customElements.define("flynt-slider-images", SliderImages, {
+  extends: "div",
+});
